84bb65865e454328a5ebeddf98997e99ce08dbc6
elkin <elkin@DESKTOP-KQKAO9U>
2025-05-02 17:09:35 +0200

    Ejercicio 2 realizado

--- src/labsemana2/Cruz_Elkin_Tiempo.java
+++ src/labsemana2/Cruz_Elkin_Tiempo.java
@@ -13,7 +13,18 @@ public class Cruz_Elkin_Tiempo {
  
  Scanner lea= new Scanner(System.in);
 
+     System.out.print("Ingrese cantidad de segundos : ");
+     int segundos_total=lea.nextInt();
+     
+int horas = segundos_total >0? segundos_total / 3600:0;
+int sobrante = segundos_total>0?segundos_total-(horas*3600):0;
+int minutos= sobrante> 0 ? sobrante / 60 : 0;
+int segundos= sobrante>0?sobrante-(minutos * 60) : 0;
 
-
- }   
+ String resultado= segundos_total<=0?"No se permite este valor": "Horas:"+horas+" Minutos:"+minutos+ " Segundos:"+segundos;
+ 
+ System.out.println(resultado);   
+         }
+ 
+    
 }

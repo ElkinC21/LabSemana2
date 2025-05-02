067b11981950e256aefa10df9cc30f95baa24656
elkin <elkin@DESKTOP-KQKAO9U>
2025-05-02 18:16:47 +0200

    push inicial ejercicio 4

---
+++ src/labsemana2/Cruz_Elkin_FECHA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labsemana2;
+import java.util.Scanner;
+/**
+ *
+ * @author elkin
+ */
+public class Cruz_Elkin_FECHA {
+ public static void main(String[]args){
+ 
+ Scanner lea=new Scanner(System.in);
+ 
+ 
+ }  
+}

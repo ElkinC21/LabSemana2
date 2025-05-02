0ce06b7d209e6ed2c9c15bea33db76df59f4424d
elkin <elkin@DESKTOP-KQKAO9U>
2025-05-02 16:17:30 +0200

    Push inicial

---
+++ src/labsemana2/Cruz_Elkin_Clases.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labsemana2;
+import java.util.Scanner;
+/**
+ *
+ * @author elkin
+ */
+public class Cruz_Elkin_Clases {
+ public static void main(String[]args){
+ 
+ Scanner lea=new Scanner(System.in);
+ 
+ 
+     
+ 
+ }   
+}

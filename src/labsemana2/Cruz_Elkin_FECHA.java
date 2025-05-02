89a4fa7a2325c7c72e030fa5282715eb1fafe637
elkin <elkin@DESKTOP-KQKAO9U>
2025-05-02 19:00:28 +0200

    Ejercicio 4 completado

--- src/labsemana2/Cruz_Elkin_FECHA.java
+++ src/labsemana2/Cruz_Elkin_FECHA.java
@@ -13,6 +13,44 @@ public class Cruz_Elkin_FECHA {
  
  Scanner lea=new Scanner(System.in);
  
+     System.out.print("Ingrese la fecha 1 utilizando el siguiente formato Dia/Mes/Año: ");
+     String fecha1=lea.nextLine();
+     System.out.print("Ingrese la fecha 2 utilizando el siguiente formato Dia/Mes/Año: ");
+     String fecha2=lea.nextLine();
+     
+     
+     int  dia1=Integer.parseInt(fecha1.substring(0,2));
+     int  mes1=Integer.parseInt(fecha1.substring(3,5));
+     int año1=Integer.parseInt(fecha1.substring(6));
+ 
+     if (dia1<1||dia1>31||mes1<1||mes1>12) {
+            System.out.println("Fecha 1 no es valida");
+            return;
+        }
+     int dia2=Integer.parseInt(fecha2.substring(0,2));
+     int mes2=Integer.parseInt(fecha2.substring(3,5));
+     int año2=Integer.parseInt(fecha2.substring(6));
+
+   if (dia2<1||dia2>31||mes2 <1||mes2> 12) {
+            System.out.println("Fecha 2 no es inválida.");
+           return;
+        }
+     int  diasfecha1= año1*360+mes1*30+dia1;
+  int diasfecha2= año2*360+mes2*30+dia2;
+  int diferencia_dias=0;
+
+  if(diasfecha1>=diasfecha2){
+diferencia_dias=diasfecha1-diasfecha2;
+ 
+}else{
+diferencia_dias=diasfecha2-diasfecha1;
+}
+     System.out.println("Primera fecha ingresada: "+fecha1);
+     System.out.println("Segunda fecha ingresada: "+fecha2);
+     System.out.println("Diferencia de dias entre las dos fechas: "+ diferencia_dias);
+ 
+ 
+ 
  
  }  
 }
